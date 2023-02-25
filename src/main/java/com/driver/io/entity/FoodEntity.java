d5edf35294ae73ffaf4db661cd79b8c6a37107e5
unknown <sadanahmad96@gmail.com>
2023-02-25 16:17:27 +0530

    1st

--- src/main/java/com/driver/io/entity/FoodEntity.java
+++ src/main/java/com/driver/io/entity/FoodEntity.java
@@ -12,16 +12,16 @@ public class FoodEntity{
 	@Id
 	@GeneratedValue
 	private long id;
-	
+
 	@Column(nullable = false)
 	private String foodId;
-	
+
 	@Column(nullable = false)
 	private String foodName;
-	
+
 	@Column(nullable = false)
 	private float foodPrice;
-	
+
 	@Column(nullable = false)
 	private String foodCategory;
 
